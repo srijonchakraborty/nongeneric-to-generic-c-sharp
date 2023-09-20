9dcde9d6571ee4f3a41961d09dfd4f73fcfd1bb4
srijonchakraborty <srijonchak@live.com>
2023-09-20 11:49:04 +0600

    First Commit

---
+++ NonGenericToGenericCSharpExample/NonGenericToGenericCSharpExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericToGenericCSharpExample
+{
+    internal class NonGenericToGenericCSharpExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("T2");  
+        }
+    }
+}
